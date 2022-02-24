82ebdbdc53132eb4936f460972571453cdb20d1d
adammason921@gmail.com <adammason921@gmail.com>
2022-02-24 21:21:06 +0700

    menyelesaikan latihan2

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,80 @@
+<?php 
+// pertemuan 2 - PHP dasar 
+// sintaks PHP
+
+// Standar Output
+// echo, print 
+//print_r= isi array
+// var_dump= menampilkan detail
+
+//var_dump("adam");
+//echo 123;
+//echo "Adam";
+//kutip " lebih OP dari pada kutip '
+
+
+
+//penulisan sintaks PHP
+//1. PHP di dalam HTML
+//2. HTML di dalam PHP
+
+
+//variable dan tipe data
+//variabel
+// tidak boleh diawalali dengan angka tapi boleh mengandung angka
+
+// $nama = "adam";
+// echo 'hallo, nama saya $nama';
+
+//operator
+//aritmatika
+// + - * / %
+
+// $x =10;
+// $y =20;
+// echo $x * $y;
+
+//penggabung string / concatenation / concat
+//.
+// $nama_depan = "Adam";
+// $nama_belakang ="Doang";
+// echo $nama_depan . " ". $nama_belakang;
+
+// assigment
+// =, +=, -=, *=, /=, %=, .=, !=
+// $x =1;
+// $x -=5;
+// echo $x;
+
+// $nama = "Adam";
+// $nama .= " ";
+// $nama .= "Doang";
+// echo $nama;
+
+
+//Perbandingan
+// <, >, <=, >=, ==
+//var_dump(1 == "1");
+
+//identitas
+//===, !==
+//var_dump(1 === "1");
+
+//Logika
+//&&, ||, !
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0);
+
+?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Hallo, selamata datang <?php echo $nama; ?></h1>
+</body>
+</html> -->
