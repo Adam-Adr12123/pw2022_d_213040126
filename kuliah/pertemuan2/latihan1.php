9e30ee5271797169757eb43de9af21a635086a8c
adammason921@gmail.com <adammason921@gmail.com>
2022-02-24 11:10:50 +0700

    mengerjakan tugas pertemuan 2

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,60 @@
+<?php
+// pertemuan 2, belajarb mengenai sintaks php
+// echo untuk mencetak nilai ke layar
+echo 10 +5;
+
+echo "<hr>";
+
+//operator
+// aritmatika
+// +, -, *, %
+
+
+echo "<hr>";
+echo 5 + 2;
+
+//perbandingaan
+// <, >, =, !=, dll
+echo "<hr>";
+echo 10 > 2;
+
+
+//variabel 
+// tidak boleh mengandung spasi
+// huruf pertama tidak boleh angka
+
+echo "<hr>";
+$apa;
+
+// snake-case: ada_apa
+// chameleon-case : AdaApa
+// kebab-case : ada-apa
+
+
+
+
+
+
+// penugasan/assigment
+echo "<hr>";
+$aa =3;
+$aa +=32;
+$aa +=4;
+
+
+
+//increment
+// ++$, --$, $++, $--
+echo "<hr>";
+$t =3;
+$t++;
+echo $t;
+
+
+// identitas 
+// ===, !==
+echo "<hr>";
+echo 10 ==="10";
+
+
+// ? ~ tild, # pound, ^ caret, | pipe, / slash, \back slash
